ea25d40a7fd1358367c9b77345edcf502581ad2b
Dhaivat Parikh <dhiavat28@gmail.com>
2017-06-06 15:24:47 +0530

    added dashboard for admin

--- application/controllers/adminlogin.php
+++ application/controllers/adminlogin.php
@@ -8,5 +8,4 @@ class Adminlogin extends CI_Controller {
 	}
 }
 
-
 ?>
--- application/views/public/admin_login.php
+++ application/views/public/admin_login.php
@@ -3,17 +3,11 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Forms</title>
+<title>Admin Log in</title>
 
 <?= link_tag('assets/css/bootstrap.min.css'); ?>
 <?= link_tag('assets/css/datepicker3.css'); ?>
 <?= link_tag('assets/css/styles.css'); ?>
-
-<!--[if lt IE 9]>
-<script src="js/html5shiv.js"></script>
-<script src="js/respond.min.js"></script>
-<![endif]-->
-
 </head>
 
 <body>
@@ -21,7 +15,7 @@
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Log in</div>
+				<div class="panel-heading">Admin Log in</div>
 				<div class="panel-body">
 					<form role="form">
 						<fieldset>
@@ -31,12 +25,14 @@
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password" value="">
 							</div>
-							<div class="checkbox">
-								<label>
-									<input name="remember" type="checkbox" value="Remember Me">Remember Me
-								</label>
+							<div class="row">
+								<div class="col-lg-2">
+									<a href="index.html" class="btn btn-primary">Login</a>
+								</div>
+								<div class="col-lg-2">
+									<a href="index.html" class="btn btn-success btn-md">Reset</a>
+								</div>
 							</div>
-							<a href="index.html" class="btn btn-primary">Login</a>
 						</fieldset>
 					</form>
 				</div>
