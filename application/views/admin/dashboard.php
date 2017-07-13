50b8d295fed0b20d8849e9c9cbf94546ec5e897f
Dhaivat Parikh <dhiavat28@gmail.com>
2017-07-14 01:45:46 +0530

    removed navbar from design

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -17,12 +17,10 @@
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
-				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use><0/svg></a></li>
-				<li class="active">Dashboard</li>
+				<li id="new-li" class="active">Dashboard</li>
 			</ol>
 		</div><!--/.row-->
 
-	<br />
 		<div class="row">
 			<div class="col-xs-6 col-md-3">
 				<div class="panel panel-default">
