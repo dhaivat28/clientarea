4ad9d7041f7dacd1b8eb30935e901a814a0b9b01
Dhaivat Parikh <dhiavat28@gmail.com>
2017-07-20 16:43:12 +0530

    Integrated php mailer in codeigniter

---
+++ application/controllers/mailtoclient.php
@@ -0,0 +1,19 @@
+<?php
+
+class Mailtoclient extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->library('email');
+		$subject = 'This is a test';
+		$message = '<p>This message has been sent for testing purposes.</p>';
+		$result = $this->email
+     ->from('aspironwebmailer@gmail.com')
+     ->reply_to('aspironweb@gmail.com')    // Optional, an account where a human being reads.
+     ->to('dhaivat28@gmail.com')
+     ->subject($subject)
+     ->message($message)
+     ->send();
+	}
+}
+?>
