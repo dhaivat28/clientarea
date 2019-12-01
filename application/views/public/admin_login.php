ae6c7432fdab91d92e5be1c41bc654b908ae65ff
Dhaivat Parikh <dhaivat28@gmail.com>
2019-12-02 00:46:17 +1100

    updated site title

--- application/views/public/admin_login.php
+++ application/views/public/admin_login.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Forms</title>
+<title>Welcome to Clientarea</title>
 
 <?= link_tag('assets/css/bootstrap.min.css'); ?>
 <?= link_tag('assets/css/datepicker3.css'); ?>
