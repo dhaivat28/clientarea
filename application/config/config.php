276312e30559d36a7f02dbb88d82846d1df22a8c
Dhaivat Parikh <dhaivat28@gmail.com>
2020-02-22 15:12:06 +1100

    sess fix

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+$config['sess_save_path'] = sys_get_temp_dir();
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
